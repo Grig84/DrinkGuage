45d0bb0b4471bb6e6c9552c40fe13a38931afea7
Will Dirkswager <Will@dirkback.com>
2024-12-11 11:38:15 -0600

    Initial Commit

---
+++ DrinkGuage/DrinkGuageApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkGuageApp.swift
+//  DrinkGuage
+//
+//  Created by Will Dirkswager on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DrinkGuageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrinkGuageTests/DrinkGuageTests.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkGuageTests.swift
+//  DrinkGuageTests
+//
+//  Created by Will Dirkswager on 12/11/24.
+//
+
+import Testing
+@testable import DrinkGuage
+
+struct DrinkGuageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
